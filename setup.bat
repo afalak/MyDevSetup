0ca8e7e9b3b1e96b61066ed09a2db2b280084bab
Falak Ahmed <afalak@users.noreply.github.com>
2017-08-20 21:15:51 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,9 +1,9 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install chocolatey-core.extension
-choco install chocolateygui
-choco install chocolatey-windowsupdate.extension
-choco install putty.install
-choco install keepass.install
+choco install chocolatey-core.extension;
+choco install chocolateygui;
+choco install chocolatey-windowsupdate.extension;
+choco install putty.install;
+choco install keepass.install;
 choco install openssh
 choco install git.install
 choco install clink
